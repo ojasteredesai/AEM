2d9242028dcd15776d9c0c7b158037b8ced83d2c
ojasteredesai <ojasteredesai@gmail.com>
2018-01-29 22:48:22 +0530

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,14 @@ declare var $ :any;
   template: `
   <div><h1>{{pageTitle}}</h1>
     <pm-products></pm-products>
-  </div>`
+  </div>`,
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit {
   pageTitle: string = 'ACME Product Management';
   ngOnInit(){
-    $(".dropdown").hover(            
+    $(".dropdown").click(            
       function() {
           $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
           $(this).toggleClass('open');        
