eac74318aa903fa0bdc85b8bb2153c39e386b591
unknown <oteredesai@VADDFZ2G102.zerochaos.local>
2018-02-16 17:52:31 +0530

    added navigation logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import {RouterModule} from '@angular/router';
+import {CourseDetailComponent} from '../course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    CourseDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'course', component:CourseDetailComponent},
+      {path: 'course/:name', component:CourseDetailComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
