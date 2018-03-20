74d9a4b015ba37138f2a03f6f87a049414201440
Ojas Teredesai <ojasteredesai@gmail.com>
2018-03-20 14:27:14 +0530

    added images

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-    templateUrl: './welcome.component.html'
+    templateUrl: './welcome.component.html',
+    styleUrls:['./welcome.component.css']
 })
 export class WelcomeComponent {
-    public pageTitle: string = 'Welcome';
+    public homeContent: string = `<p>
+    <b>AEM - </b> Academy of Emergency  Medicine is an academy to prepare and train health care providers for Emergency Medical Service.
+    <br/><br/>
+    <b>Affiliation - </b>
+    <br/>
+    LIHS (Life supporters Institute of Health Science) - A Premier Institute for Providing To Helth care providers Mumbai.
+    <br/>
+    AHA (American Heart Association) -  Recognised Training Centre.
+    </p>`
+    ;
 }
